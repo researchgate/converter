3b23a96e68d4df849b2578074231b549aca5d93c
Michael Diodone <mdio@users.noreply.github.com>
2020-02-27 17:43:09 +0100

    Make library PHP 7.3/7.4 compatible

--- library/bibtexParse/PARSEENTRIES.php
+++ library/bibtexParse/PARSEENTRIES.php
@@ -245,7 +245,7 @@ class PARSEENTRIES
 			$oldString = substr_replace($oldString, '', $pos, strlen($value));
 		}
 		$rev = strrev(trim($oldString));
-		if($rev{0} != ',')
+		if($rev[0] != ',')
 			$oldString .= ',';
 		$keys = preg_split("/=,/", $oldString);
 		// 22/08/2004 - Mark Grimshaw
@@ -257,7 +257,7 @@ class PARSEENTRIES
 			$value = trim(array_shift($values));
 			$rev = strrev($value);
 			// remove any dangling ',' left on final field of entry
-			if($rev{0} == ',')
+			if($rev[0] == ',')
 				$value = rtrim($value, ",");
 			if(!$value)
 				continue;
@@ -313,12 +313,12 @@ class PARSEENTRIES
 // Remove delimiters from a string
 	function removeDelimiters($string)
 	{
-		if($string  && ($string{0} == "\""))
+		if($string  && ($string[0] == "\""))
 		{
 			$string = substr($string, 1);
 			$string = substr($string, 0, -1);
 		}
-		else if($string && ($string{0} == "{"))
+		else if($string && ($string[0] == "{"))
 		{
 			if(strlen($string) > 0 && $string[strlen($string)-1] == "}")
 			{
